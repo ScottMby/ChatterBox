8047532e44fe175351bcdfa7bf6ed0fd1dd669e4
SCOTT MUMBY <smumby2003@gmail.com>
2024-08-17 15:26:36 +0100

    Implement Logger DI

--- ChatterBox/Controllers/UserController.cs
+++ ChatterBox/Controllers/UserController.cs
@@ -5,6 +5,12 @@ namespace ChatterBox.Controllers
 {
     public class UserController : Controller
     {
-        
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+            throw new NotImplementedException();
+        }
     }
 }
