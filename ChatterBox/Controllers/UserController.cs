dbf8e37e46dd1dbba7085a4cb8fdc99b99fe3cf1
SCOTT MUMBY <smumby2003@gmail.com>
2024-08-17 16:25:51 +0100

    Set up register and test structure

--- ChatterBox/Controllers/UserController.cs
+++ ChatterBox/Controllers/UserController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ChatterBox.Controllers
 {
+    [ApiController]
+    [Route("user/[controller]")]
     public class UserController : Controller
     {
         private readonly ILogger<UserController> _logger;
@@ -10,6 +13,12 @@ namespace ChatterBox.Controllers
         public UserController(ILogger<UserController> logger)
         {
             _logger = logger;
+        }
+
+        [HttpPost("register")]
+        [Authorize]
+        public async Task Register()
+        {
             throw new NotImplementedException();
         }
     }
---
+++ ChatterBoxTests/Controllers/TestUserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatterBoxTests.Controllers
+{
+    public class TestUserController
+    {
+        [Fact]
+        public async Task Post_RegisterUser_OnSuccess_ReturnStatusCode200()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
